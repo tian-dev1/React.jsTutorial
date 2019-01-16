e68a78fafd7c9a638a36769bc3a6d2169558b030
Cristian Camilo <c.arevalo@mensajerosurbanos.com>
2019-01-16 15:08:58 -0500

    Empezamos con el tutorial de react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Title from './components/Title';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <section>
+        <div>
+          <div>
+            <Title></Title>
+            <button>Crear cuenta gratuita</button>
+            <div>
+              <ul>
+                <li>
+                  <h3>Califcaciones con emociones</h3>
+                   <p>Califica tus lugares con experiencias, no con números</p>
+                </li>
+                <li>
+                  <h3>¿Sin internet? sin problemas</h3>
+                   <p>Places funciona sin interner o en conexiones lentas</p>
+                </li>
+                <li>
+                  <h3>Tus lugares favoritos</h3>
+                   <p>Define la lista de tus sitios favoritos</p>
+                </li>
+              </ul> 
+            </div>
+          </div>
+        </div>
+      </section>
     );
   }
 }

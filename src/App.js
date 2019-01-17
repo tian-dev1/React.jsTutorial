d01f6cc97ac2305934ff538e02a05d396fcce316
Cristian Camilo <c.arevalo@mensajerosurbanos.com>
2019-01-17 17:31:35 -0500

    Se cambio la ubicación de los archivos principales se agrego framework material-ui y se insertaron algunas imagenes y un boton

--- src/App.js
+++ src/App.js
@@ -1,35 +1,57 @@
+//Librerias externas
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
+import { Card, CardText } from 'material-ui/Card'
+import {indigo400,redA400,lightBlue400,amberA400} from 'material-ui/styles/colors';
+
+//Mis componentes
 import Title from './components/Title';
 
+//Mis hojas de estilo
+import './App.css';
+
+
 class App extends Component {
+
   render() {
     return (
-      <section>
-        <div>
-          <div>
-            <Title></Title>
-            <button>Crear cuenta gratuita</button>
-            <div>
-              <ul>
-                <li>
-                  <h3>Califcaciones con emociones</h3>
-                   <p>Califica tus lugares con experiencias, no con números</p>
-                </li>
-                <li>
-                  <h3>¿Sin internet? sin problemas</h3>
-                   <p>Places funciona sin interner o en conexiones lentas</p>
-                </li>
-                <li>
-                  <h3>Tus lugares favoritos</h3>
-                   <p>Define la lista de tus sitios favoritos</p>
-                </li>
-              </ul> 
-            </div>
+      <MuiThemeProvider>
+        <div className="Header-background">
+          <div style={{"width":"80%", "margin":"0 auto"}}>
+          <div className="Header-main">
+              <Title></Title>
+              <RaisedButton label="Crear cuenta gratuita" primary={true} />
+              <img className="Header-illustration" src={'/images/imagen0.png'}></img>
+          </div>       
           </div>
         </div>
-      </section>
+            <div style={{'backgroundColor': indigo400, 'padding': '50px'}}>
+              <ul>
+                <Card className="Header-Benefit">
+                  <CardText style={{'backgroundColor': redA400}}>
+                    <img src={'/images/imagen0.png'}></img>
+                    <h3>Califcaciones con emociones</h3>
+                    <p>Califica tus lugares con experiencias, no con números</p>
+                  </CardText>
+                </Card>
+                <Card className="Header-Benefit">
+                  <CardText style={{'backgroundColor': redA400}}>
+                    <img src={'/images/imagen1.png'}></img>
+                    <h3>¿Sin internet? sin problemas</h3>
+                    <p>Places funciona sin interner o en conexiones lentas</p>
+                  </CardText>
+                </Card>
+                <Card className="Header-Benefit">
+                  <CardText style={{'backgroundColor': redA400}}>
+                    <img src={'/images/imagen2.png'}></img>
+                    <h3>Tus lugares favoritos</h3>
+                    <p>Define la lista de tus sitios favoritos</p>
+                  </CardText>
+                </Card>
+              </ul> 
+            </div>
+      </MuiThemeProvider>
     );
   }
 }

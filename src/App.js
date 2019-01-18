9c103de2c77fa99a6410e3d0f46d1e164a704c13
Cristian Camilo <c.arevalo@mensajerosurbanos.com>
2019-01-18 12:06:04 -0500

    se cambio icono principal, se cambio color del boton y se agregaron 3 card déspues del icono principal para organizar los beneficios de la página

--- src/App.js
+++ src/App.js
@@ -21,35 +21,63 @@ class App extends Component {
           <div style={{"width":"80%", "margin":"0 auto"}}>
           <div className="Header-main">
               <Title></Title>
-              <RaisedButton label="Crear cuenta gratuita" primary={true} />
+              <RaisedButton label="Crear cuenta gratuita" secondary={true} />
               <img className="Header-illustration" src={'/images/imagen0.png'}></img>
-          </div>       
+          </div>   
+
+          <div>
+          <ul>
+                <Card className="Header-Benefit">
+                  <CardText>
+                    <div className="row">
+                      <div className="Header-Benefit-image" style={{'backgroundColor': redA400}}>
+                        <img src={'/images/imagen1.png'}/>
+                      </div>
+                      <div className="Header-Benefit-content">
+                        <h3>Califcaciones con emociones</h3>
+                        <p>Califica tus lugares con experiencias, no con números</p>
+                      </div>
+                    </div>
+                  </CardText>
+                </Card>
+                
+                <Card className="Header-Benefit">
+                  <CardText>
+                    <div className="row">
+                      <div className="Header-Benefit-image" style={{'backgroundColor': lightBlue400}}>
+                        <img src={'/images/imagen2.png'}/>
+                      </div>
+                      <div className="Header-Benefit-content">
+                        <h3>¿Sin internet? sin problemas</h3>
+                        <p>Places funciona sin interner o en conexiones lentas</p>
+                      </div>
+                    </div>
+                  </CardText>
+                </Card>
+
+                <Card className="Header-Benefit">
+                  <CardText>
+                    <div className="row">
+                      <div className="Header-Benefit-image" style={{'backgroundColor': amberA400}}>
+                        <img src={'/images/imagen3.png'}/>
+                      </div>
+                      <div className="Header-Benefit-content">
+                        <h3>Tus lugares favoritos</h3>
+                        <p>Define la lista de tus sitios favoritos</p>
+                      </div>
+                    </div>
+                  </CardText>
+                </Card>
+              </ul>
+          </div>
+
+
+
+
           </div>
         </div>
             <div style={{'backgroundColor': indigo400, 'padding': '50px'}}>
-              <ul>
-                <Card className="Header-Benefit">
-                  <CardText style={{'backgroundColor': redA400}}>
-                    <img src={'/images/imagen0.png'}></img>
-                    <h3>Califcaciones con emociones</h3>
-                    <p>Califica tus lugares con experiencias, no con números</p>
-                  </CardText>
-                </Card>
-                <Card className="Header-Benefit">
-                  <CardText style={{'backgroundColor': redA400}}>
-                    <img src={'/images/imagen1.png'}></img>
-                    <h3>¿Sin internet? sin problemas</h3>
-                    <p>Places funciona sin interner o en conexiones lentas</p>
-                  </CardText>
-                </Card>
-                <Card className="Header-Benefit">
-                  <CardText style={{'backgroundColor': redA400}}>
-                    <img src={'/images/imagen2.png'}></img>
-                    <h3>Tus lugares favoritos</h3>
-                    <p>Define la lista de tus sitios favoritos</p>
-                  </CardText>
-                </Card>
-              </ul> 
+               
             </div>
       </MuiThemeProvider>
     );

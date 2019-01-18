ad7e12c003f454d3b6350e04824ea16dcd23811e
Cristian Camilo <c.arevalo@mensajerosurbanos.com>
2019-01-18 17:45:29 -0500

    Se agrego carpeta requests con un json para mostar en la página

--- src/App.js
+++ src/App.js
@@ -2,17 +2,37 @@
 import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
-import { Card, CardText } from 'material-ui/Card'
+import { Card, CardText, CardMedia, CardTitle } from 'material-ui/Card'
 import {indigo400,redA400,lightBlue400,amberA400} from 'material-ui/styles/colors';
 
 //Mis componentes
 import Title from './components/Title';
+import data from './requests/places';
 
 //Mis hojas de estilo
 import './App.css';
 
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    console.log(data);
+  }
+    places(){
+      return data.places.map(place=>{
+        return(
+          <card className="col-xs-12 col-sm-4">
+            <CardMedia>
+              <img src={place.imageUrl}/>
+            </CardMedia>
+            <CardTitle title={place.title}></CardTitle>
+            <CardText>{place.description}</CardText>
+          </card>
+        );
+      })
+    }
+    
 
   render() {
     return (
@@ -77,7 +97,9 @@ class App extends Component {
           </div>
         </div>
             <div style={{'backgroundColor': indigo400, 'padding': '50px'}}>
-               
+              <div className="row">
+                {this.places()}
+              </div> 
             </div>
       </MuiThemeProvider>
     );
